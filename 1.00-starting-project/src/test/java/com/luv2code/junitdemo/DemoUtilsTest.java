77cf9f2c5b6e390060e5e91b137756279b5d02ef
bowwow2115 <bowwow2115@gmail.com>
2022-09-22 21:59:52 +0900

    add, null Test 추가

--- 1.00-starting-project/src/test/java/com/luv2code/junitdemo/DemoUtilsTest.java
+++ 1.00-starting-project/src/test/java/com/luv2code/junitdemo/DemoUtilsTest.java
@@ -1,16 +1,35 @@
 package com.luv2code.junitdemo;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class DemoUtilsTest {
-    
+   
+    DemoUtils demoUtils;
+
+    @BeforeEach
+    void setupBefoeEach() {
+        demoUtils = new DemoUtils();
+        System.out.println("@BeforEach");
+    }
+
     @Test
     void tesetEqualsAndNotEquals() {
-        DemoUtils demoUtils = new DemoUtils();
-        
+
+        System.out.println("Running test: tesetEqualsAndNotEquals");
+
         assertEquals(6, demoUtils.add(2, 4));
         assertNotEquals(4, demoUtils.add(2, 4));
+    }
+
+    @Test
+    void testNullAndNotNull() {
+
+        System.out.println("Running test: testNullAndNotNull");
+
+        assertNull(demoUtils.checkNull(null));
+        assertNotNull(demoUtils.checkNull("notNull"));
     }
 }
