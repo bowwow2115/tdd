dccfb39a2f02b8fc98aeffa17c0e3dc2b8a7a354
bowwow2115 <bowwow2115@gmail.com>
2022-09-29 23:56:44 +0900

    StringBuilder 사용하여 변경

--- 1.00-starting-project/src/main/java/com/luv2code/tdd/FizzBuzz.java
+++ 1.00-starting-project/src/main/java/com/luv2code/tdd/FizzBuzz.java
@@ -1,20 +1,37 @@
 package com.luv2code.tdd;
 
 class FizzBuzz {
+//    public static String compute(int i) {
+//
+//        if ((i % 3 == 0) && (i % 5 == 0)) {
+//            return "FizzBuzz";
+//        }
+//
+//        else if (i % 3 == 0) {
+//            return "Fizz";
+//        }
+//
+//        else if (i % 5 == 0) {
+//            return "Buzz";
+//        }
+//
+//        return Integer.toString(i);
+//    }
+
     public static String compute(int i) {
 
-        if ((i % 3 == 0) && (i % 5 == 0)) {
-            return "FizzBuzz";
+        StringBuilder result = new StringBuilder();
+
+        if (i % 3 == 0) {
+            result.append("Buzz");
         }
 
-        else if (i % 3 == 0) {
-            return "Fizz";
+        if (i % 5 == 0) {
+            result.append("Buzz");
         }
 
-        else if (i % 5 == 0) {
-            return "Buzz";
-        }
+        result.append(i);
 
-        return Integer.toString(i);
+        return result.toString();
     }
 }
