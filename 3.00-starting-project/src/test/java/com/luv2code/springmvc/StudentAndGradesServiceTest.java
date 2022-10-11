a76dc0e6001ded79659d2b203694847eafea5da0
bowwow2115 <bowwow2115@gmail.com>
2022-10-11 23:41:52 +0900

    NullCheck Test추가

--- 3.00-starting-project/src/test/java/com/luv2code/springmvc/StudentAndGradesServiceTest.java
+++ 3.00-starting-project/src/test/java/com/luv2code/springmvc/StudentAndGradesServiceTest.java
@@ -3,10 +3,14 @@ package com.luv2code.springmvc;
 import com.luv2code.springmvc.models.CollegeStudent;
 import com.luv2code.springmvc.repository.StudentDao;
 import com.luv2code.springmvc.service.StudentAndGradeService;
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.mockito.Mock;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.test.context.TestPropertySource;
 
 import static org.junit.jupiter.api.Assertions.*;
@@ -21,6 +25,20 @@ public class StudentAndGradesServiceTest {
     @Autowired
     private StudentDao studentDao;
 
+    @Autowired
+    private JdbcTemplate jdbc;
+
+    @BeforeEach
+    public void setupDatabase() {
+        jdbc.execute("insert into student(id, firstname, lastname, email_address)" +
+                "values (1, 'SeungHun', 'Park', 'bowwow12@naver.com')");
+    }
+
+    @AfterEach
+    public void setUpAfterTransaction() {
+        jdbc.execute("DELETE FROM student");
+    }
+
     @Test
     public void createStudentService() {
 
@@ -29,5 +47,11 @@ public class StudentAndGradesServiceTest {
         CollegeStudent student = studentDao.findByEmailAddress("bowwow12@naver.com");
 
         assertEquals("bowwow12@naver.com", student.getEmailAddress());
+    }
+
+    @Test
+    public void isStudentNullCheck() {
+        assertTrue(studentService.checkIfStudentIsNull(1));
+        assertFalse(studentService.checkIfStudentIsNull(0));
     }
 }
