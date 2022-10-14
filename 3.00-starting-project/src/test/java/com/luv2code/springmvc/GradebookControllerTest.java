15e5cac2e3926344f00eb41fafef4b811e4b1503
bowwow2115 <bowwow2115@gmail.com>
2022-10-14 23:07:56 +0900

    MVC Mock Test 추가

---
+++ 3.00-starting-project/src/test/java/com/luv2code/springmvc/GradebookControllerTest.java
@@ -0,0 +1,64 @@
+package com.luv2code.springmvc;
+
+import com.luv2code.springmvc.models.CollegeStudent;
+import com.luv2code.springmvc.models.GradebookCollegeStudent;
+import com.luv2code.springmvc.service.StudentAndGradeService;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mockito.Mock;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.test.context.TestPropertySource;
+import org.springframework.test.web.servlet.MockMvc;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.when;
+
+@TestPropertySource("/application.properties")
+@AutoConfigureMockMvc
+public class GradebookControllerTest {
+
+    @Autowired
+    private JdbcTemplate jdbc;
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Mock
+    private StudentAndGradeService studentAndGradeServiceMock;
+
+    @BeforeEach
+    public void beforeEach() {
+        jdbc.execute("insert into student(id, firstname, lastname, email_address)" +
+                "values (1, 'SeungHun', 'Park', 'bowwow12@naver.com')");
+    }
+
+    @Test
+    public void getStudentHttpRequest() throws Exception {
+
+        CollegeStudent studentOne = new GradebookCollegeStudent("Eric", "Roby",
+                "eric_roby@luv2code_school.com");
+
+        CollegeStudent studentTwo = new GradebookCollegeStudent("SeungHun", "Park",
+                "bowwow12@naver.com");
+
+        List<CollegeStudent> collegeStudentList = new ArrayList<>(Arrays.asList(studentOne, studentTwo));
+
+        when(studentAndGradeServiceMock.getGradebook()).thenReturn(collegeStudentList);
+
+        assertIterableEquals(collegeStudentList, studentAndGradeServiceMock.getGradebook());
+
+    }
+
+    @AfterEach
+    public void setUpAfterTransaction() {
+        jdbc.execute("DELETE FROM student");
+    }
+
+}
