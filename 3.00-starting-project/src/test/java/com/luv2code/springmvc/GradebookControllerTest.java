426000a2088f64166f9994a2190dd682ee1a4e55
bowwow2115 <bowwow2115@gmail.com>
2022-10-17 23:29:45 +0900

    createStudentHttpRequest 테스트, getStudentHttpRequest 테스트 추가

--- 3.00-starting-project/src/test/java/com/luv2code/springmvc/GradebookControllerTest.java
+++ 3.00-starting-project/src/test/java/com/luv2code/springmvc/GradebookControllerTest.java
@@ -4,14 +4,22 @@ import com.luv2code.springmvc.models.CollegeStudent;
 import com.luv2code.springmvc.models.GradebookCollegeStudent;
 import com.luv2code.springmvc.service.StudentAndGradeService;
 import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.mockito.Mock;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
 import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.mock.web.MockHttpServletRequest;
 import org.springframework.test.context.TestPropertySource;
+import org.springframework.test.web.ModelAndViewAssert;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.MvcResult;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.web.servlet.ModelAndView;
 
 import java.util.ArrayList;
 import java.util.Arrays;
@@ -19,10 +27,15 @@ import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.Mockito.when;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @TestPropertySource("/application.properties")
 @AutoConfigureMockMvc
+@SpringBootTest
 public class GradebookControllerTest {
+
+    private static MockHttpServletRequest request;
 
     @Autowired
     private JdbcTemplate jdbc;
@@ -33,9 +46,17 @@ public class GradebookControllerTest {
     @Mock
     private StudentAndGradeService studentAndGradeServiceMock;
 
+    @BeforeAll
+    public static void setup() {
+        request = new MockHttpServletRequest();
+        request.setParameter("firstname", "SH");
+        request.setParameter("lastname", "Park");
+        request.setParameter("email_address", "bowwow12@naver.com");
+    }
+
     @BeforeEach
     public void beforeEach() {
-        jdbc.execute("insert into student(id, firstname, lastname, email_address)" +
+        jdbc.execute("insert into student(id, firstname, lastname, email_address) " +
                 "values (1, 'SeungHun', 'Park', 'bowwow12@naver.com')");
     }
 
@@ -53,6 +74,23 @@ public class GradebookControllerTest {
         when(studentAndGradeServiceMock.getGradebook()).thenReturn(collegeStudentList);
 
         assertIterableEquals(collegeStudentList, studentAndGradeServiceMock.getGradebook());
+
+        MvcResult mvcResult = mockMvc.perform(MockMvcRequestBuilders.get("/"))
+                .andExpect(status().isOk()).andReturn();
+
+        ModelAndView mav = mvcResult.getModelAndView();
+
+        ModelAndViewAssert.assertViewName(mav, "index");
+    }
+
+    @Test
+    public void createStudentHttpRequest() throws Exception {
+        MvcResult mvcResult = this.mockMvc.perform(post("/")
+                .contentType(MediaType.APPLICATION_JSON)
+                .param("firstname", request.getParameter("firstname"))
+                .param("lastname", request.getParameter("lastname"))
+                .param("emailAddress", request.getParameter("emailAddress")))
+                .andExpect(status().isOk()).andReturn();
 
     }
 
